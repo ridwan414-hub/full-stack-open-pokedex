e651a3a843b78de43dda5e17e1db18597e2713a2
ridwan <asifmahmoud414@gmail.com>
2024-10-13 17:07:35 +0600

    test with canary deployment strategy and health check api

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/version', (req, res) => {
-  res.send('4')
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 // start app in a wrong port
