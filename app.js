bf377398a7b3d6cfa30c16de0cb7823a72b1fc3d
ridwan <asifmahmoud414@gmail.com>
2024-10-13 20:00:19 +0600

    test with error

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 5000 //5000 used for testing and PORT 3000(from
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw ('error...  ')
   res.send('ok')
 })
 
