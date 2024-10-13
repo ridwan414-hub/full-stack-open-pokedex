442136bee335ea085813f35b7e3993e282cb43f2
ridwan <asifmahmoud414@gmail.com>
2024-10-13 15:16:36 +0600

    back to common js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'cypress'
+const { defineConfig } = require('cypress')
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
     supportFile: false,
     baseUrl: 'http://localhost:5000',
